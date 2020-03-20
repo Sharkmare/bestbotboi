84148d3d22266688a6f984d8bb7f1ba71e3f7c8a
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2020-03-20 07:48:10 +0100

    Update boibot.js

--- boibot.js
+++ boibot.js
@@ -54,11 +54,7 @@ bot.connect(
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-/*
-client.on('channelUpdate', e =>{ if(e.guild.id == '180538463655821312') {CM('323951163752054785',"A Channel was moved:\nChannel "+e.name+"\n Moved to "+e.position)
-									//console.log(e)
-									}
-			       })*/
+
 bot.isFirstConnect = 1
 bot.Dispatcher.on("GATEWAY_READY", e =>
 {
@@ -81,3 +77,170 @@ bot.Dispatcher.on("DISCONNECTED", e =>
 {
 	return console.log("Connection lost", console.log(servers))
 });
+
+function CM(channel, message){bot.Channels.get(channel).sendMessage(message)} //Short for Channel message. CM(ChannelID,Message)
+
+bot.Dispatcher.on("MESSAGE_CREATE", e =>
+{
+	var antiecho;
+	var guild;
+	var msg;
+	if (e.message.guild)
+	{
+		guild = e.message.guild.name
+	}
+	if (e.message)
+	{
+		msg = e.message
+	}
+
+	//Fleet Legacy Handling
+	if (msg.content.split("")[0] == "-")
+	{
+		console.log("Before Manipulation"+e.message.author.username + " | " + e.message.content + " | " + guild)
+		suffix = msg.content.split(" ")
+		trigger = suffix[0].replace("-", "")
+		suffix = suffix.join(" ")
+		suffix = suffix.replace("-"+trigger, "").replace(" ","")
+		console.log("Suffix: "+suffix)
+		execute = Commands.filter(e => e.name == trigger.toLowerCase())
+		if (execute.length < 1)
+		{
+			CommandsWithAliases = Commands.filter(e => e.aliases) //Ignore commands without aliases
+			execute = CommandsWithAliases.filter(e => e.aliases.includes(trigger.toLowerCase()))
+		}
+		if (execute.length > 1) {CM(logchannel,"Duplicate commands found:\n"+msg.content)}
+		if (execute.length > 0)
+		{
+			if (execute[0].permFlag)
+			{flags = hasPerm(msg.guild.id, msg.author.id)
+				if(flags[0]) {}
+			 	else if(flags[execute[0].permFlag])
+				{}else {return msg.reply("Necesary Flag not present, Flag needed: " +execute[0].permFlag+ "\nFor reference of Flag ids use _hasPerms")}
+			}
+			if (execute[0].noDM && !msg.guild)
+			{
+				return msg.reply("This Command can not be executed in DMs")
+			}
+			if (execute[0].level == "master" && msg.author.id != botowner)
+			{
+				return
+			}
+			//console.log(suffix)
+			try
+			{
+				execute[0].fn(msg, suffix, bot, client)
+			}
+			catch (err)
+			{
+				CM(logchannel, err)
+			}
+			antiecho = 1;
+		}
+	}
+	
+});
+
+function relaymedaddy(message)
+{
+	var me = "201983882625548299";
+	bot.Users.get(me).openDM()
+		.then(function(x)
+		{
+			x.sendMessage(message)
+			return x.close()
+		})
+}
+function protoDM(ID, MESSAGE, bot)
+{
+	bot.Users.get(ID).openDM() //opens the dm
+		.then(function(x)
+		{ //passes the info of the opened DM into x
+			x.sendMessage(MESSAGE) //sends a message to x
+			x.close() //closes the DM
+		})
+		.catch(function(error)
+		{
+			// handle error
+			protoDM('201983882625548299', error)
+		})
+}
+
+Commands.push(
+{
+	name: 'info',
+	help: "I'll print some information about me.",
+	timeout: 10,
+	level: 0,
+	fn: function(msg, suffix, bot)
+	{
+		var owner = bot.Users.get(botowner).username
+		var field = [
+		{
+			name: 'Servers Connected',
+			value: '```\n' + bot.Guilds.length + '```',
+			inline: true
+		},
+		{
+			name: 'Users Known',
+			value: '```\n' + bot.Users.length + '```',
+			inline: true
+		},
+		{
+			name: 'Channels Connected',
+			value: '```\n' + bot.Channels.length + '```',
+			inline: true
+		},
+		{
+			name: 'Private Channels',
+			value: '```\n' + bot.DirectMessageChannels.length + '```',
+			inline: true
+		},
+		{
+			name: 'Messages Received',
+			value: '```\n' + bot.Messages.length + '```',
+			inline: true
+		},
+		{
+			name: 'Version',
+			value: '```\n' + version + '```',
+			inline: true
+		},
+		{
+			name: 'Owner',
+			value: '```\n' + owner + '```',
+			inline: true
+		}]
+		
+		msg.channel.sendMessage('', false,
+		{
+			color: 0x3498db,
+			author:
+			{
+				icon_url: bot.User.avatarURL,
+				name: `${bot.User.username}#${bot.User.discriminator} (${bot.User.id})`
+			},
+			title: ``,
+			timestamp: new Date(),
+			fields: field,
+			description: '',
+			footer:
+			{
+				//text: `Online for ${getUptime()}`
+			}
+		})
+	}
+})
+Commands.push(
+{
+	name: 'dm',
+	help: "Direct Message handler",
+	hidden: true,
+	level: 'master',
+	fn: function(msg, suffix, bot)
+	{
+		suffix = suffix.split("|")
+		protoDM(suffix[0], suffix[1], bot)
+	}
+})
+
