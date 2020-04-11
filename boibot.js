9f02ee1c65056d947695df3b7ee9185c19a148b9
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2020-04-11 13:08:55 +0200

    woops Config.vrchat. not just vrchat.

--- boibot.js
+++ boibot.js
@@ -1,4 +1,4 @@
-const version = `BadBoi V2.1666`
+const version = `BadBoi V2.2666`
 
 try {
     Config = require('./config.json')
@@ -510,7 +510,7 @@ snomposter(["300131285634908163","559624972742688769"],
 function statusliveupdate(delay) {
     setTimeout(function() {
           var URL = "https://api.vrchat.cloud/api/1/worlds/wrld_05be1d4a-72ae-489b-93bd-489d2b78abc5?apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26";
-    axios.get(URL, {auth:{username: vrchat.user,password: vrchat.password}})
+    axios.get(URL, {auth:{username: Config.vrchat.user,password: Config.vrchat.password}})
         .then(response => {console.log(response)
 	    bot.Channels.get('691022282981900328').update("world favs: "+response.data.favorites)
 	    bot.Channels.get('691022699048206386').update("world visits: "+response.data.visits)
